33e4c1aba6f2f62fcaa24796eff69854435f4c1a
SyedShahil <157971911+SyedShahil@users.noreply.github.com>
2024-09-01 22:30:53 +0530

    Add files via upload

---
+++ Jump_Game2.java
@@ -0,0 +1,17 @@
+class Jump_Game2{
+    public int jump(int[] nums) {
+        int cnt=0;
+        int l=0;
+        int r=0;
+        while(r<nums.length-1){
+            int far=0;
+            for(int i=l;i<=r;i++){
+                far=Math.max((i+nums[i]),far);
+            }
+            l=r+1;
+            r=far;
+            cnt++;
+        }
+        return cnt;
+    }
+}
