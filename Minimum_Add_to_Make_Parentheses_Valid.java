211b19fbebbaa7bb7b0e6c314e91f19034419492
SyedShahil <157971911+SyedShahil@users.noreply.github.com>
2024-10-09 22:09:41 +0530

    Add files via upload

---
+++ Minimum_Add_to_Make_Parentheses_Valid.java
@@ -0,0 +1,21 @@
+class  Minimum_Add_to_Make_Parentheses_Valid{
+    public int minAddToMakeValid(String s) {
+        int open=0;
+        int close=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                open++;
+            }
+            else{
+                if(open>0){
+                    open--;
+                }
+                else{
+                close++;
+                }
+            }
+        }
+        return open+close;
+        }
+}
